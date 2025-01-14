f4f401301d3bb9d24e30ac6ba969064b85119cc7
OleksandrDrob <oleksandr.drobyshev@jadexconsulting.com>
2025-01-14 11:14:00 +0200

    added package

---
+++ Framework/MyXC/Package.swift
@@ -0,0 +1,21 @@
+// swift-tools-version: 6.0
+import PackageDescription
+
+let package = Package(
+    name: "MyXC",
+    platforms: [
+        .iOS(.v15) // Adjust platform as needed
+    ],
+    products: [
+        .library(
+            name: "MyXC",
+            targets: ["MyXC"]
+        )
+    ],
+    targets: [
+        .binaryTarget(
+            name: "MyXC",
+            path: "./build/MyXC.xcframework" // Relative path to the xcframework
+        )
+    ]
+)
